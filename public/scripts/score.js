460b9e1001d7dac704e57cb3da36338d4441d7b7
Stephanie Viveros <sviveros05@gmail.com>
2020-11-01 15:56:00 -0600

    Fix syntax

--- public/scripts/score.js
+++ public/scripts/score.js
@@ -1,11 +1,11 @@
 
 const zeroScore = () => {
-    return score = 0;
+	return score = 0;
 }
 
 
 const incrementScore = (score) => {
-    return score += 1;
+  return score += 1;
 }
 
 module.exports = { incrementScore: incrementScore, zeroScore: zeroScore};
--- public/scripts/trivia.js
+++ public/scripts/trivia.js
@@ -3,14 +3,14 @@ const triviaData = require('../../data/Apprentice_TandemFor400_Data.json');
 const generateRandomQuestions = () => {
     const data = []
     let triviaDataCopy = [...triviaData]
-   
-        for (let i = 0; i < 10; i++){
-            // Generate Random Index up to array length
-            const index = Math.floor(Math.random() * triviaDataCopy.length);
-            // Push to data array
-            data.push(triviaDataCopy[index]);
-            // Remove from json copy
-            triviaDataCopy.splice(index, 1)        
+
+	for (let i = 0; i < 10; i++){
+		// Generate Random Index up to array length
+		const index = Math.floor(Math.random() * triviaDataCopy.length);
+		// Push to data array
+		data.push(triviaDataCopy[index]);
+		// Remove from json copy
+		triviaDataCopy.splice(index, 1)
     }
 
     return data;
