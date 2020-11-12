dde53d1f7c9ec10ec76daf067c0012ec96bce212
Stephanie Viveros <sviveros05@gmail.com>
2020-11-11 20:40:06 -0600

    Fix syntax

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ module.exports = ({ data, resetZero, incrementNum}) => {
   let score;
   let jsonData;
     router.post('/questions/:id', (req, res) => {
-			/**
-			 * Question Results
-			 * Check if user submitted correct answer
-			 * 	if so increment score and display the answer, question and current score
-			 * else do not increment score but display aforementioned
-			 */
+		/**
+		 * Question Results
+		 * Check if user submitted correct answer
+		 * 	if so increment score and display the answer, question and current score
+		 * else do not increment score but display aforementioned
+		 */
         try {
             let data = jsonData[parseInt(req.params.id)];
             if (data['correct'] === req.body.answer){
@@ -22,63 +22,63 @@ module.exports = ({ data, resetZero, incrementNum}) => {
                     pageTitle: 'Correct'
                 });
             } else {
-							res.status(200).render('message',{
-								message: `<h1>INCORRECT</h1><p>Sorry, <bold>${data['correct']}</bold>  is the correct answer to:\n ${data['question']} \n Your Score is currently ${score}</p>`,
-								button: `<a href="/questions/${incrementNum(parseInt(req.params.id))}" role="button" class="btn btn-primary">Next Question</a>`,
-								pageTitle: 'Incorrect'
-							});
+				res.status(200).render('message',{
+					message: `<h1>INCORRECT</h1><p>Sorry, <bold>${data['correct']}</bold>  is the correct answer to:\n ${data['question']} \n Your Score is currently ${score}</p>`,
+					button: `<a href="/questions/${incrementNum(parseInt(req.params.id))}" role="button" class="btn btn-primary">Next Question</a>`,
+					pageTitle: 'Incorrect'
+				});
             }
         } catch (error) {
-					res.status(400).render('message', {
-						message: `<h1>Error/h1><p>Something went wrong</p>`,
-						button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
-						pageTitle: 'Error'
-					});
-					console.error(error);
+			res.status(400).render('message', {
+				message: `<h1>Error/h1><p>Something went wrong</p>`,
+				button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
+				pageTitle: 'Error'
+			});
+			console.error(error);
         }
     });
 
     router.get('/questions/:id', (req, res) => {
-			/**
-			 * Question Page
-			 * Index into the array at params.id
-			 * Check if id is equal to 10 if so display game over and  final results
-			 * Else display question and answers
-			 *	 Udate iteration
-			 */
+		/**
+		 * Question Page
+		 * Index into the array at params.id
+		 * Check if id is equal to 10 if so display game over and  final results
+		 * Else display question and answers
+		 *	 Udate iteration
+			*/
         try {
             if (parseInt(req.params.id) === 10){
-							res.status(200).render('message', {
-								message: `<h1>Game Over</h1><p>Congrats on finishing the game!\n You got ${score} out of 10 questions correct.</p>`,
-								button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
-								pageTitle: 'Finished'
-							});
+				res.status(200).render('message', {
+					message: `<h1>Game Over</h1><p>Congrats on finishing the game!\n You got ${score} out of 10 questions correct.</p>`,
+					button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
+					pageTitle: 'Finished'
+				});
             } else {
                 let currentIteration = incrementNum(parseInt(req.params.id));
                 let data = jsonData[parseInt(req.params.id)];
 
                 if (data){
-									res.status(200).render('question', {
-										pageTitle: `Question ${incrementNum(parseInt(req.params.id))}`,
-										data: data,
-										num: parseInt(req.params.id),
-										id: currentIteration
-									});
+					res.status(200).render('question', {
+						pageTitle: `Question ${incrementNum(parseInt(req.params.id))}`,
+						data: data,
+						num: parseInt(req.params.id),
+						id: currentIteration
+					});
                 } else {
-									res.status(200).render('message', {
-										message: `<h1>Error</h1><p>No question with an id of ${req.params.id} was found.</p>`,
-										button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
-										pageTitle: 'Error'
-								});
-							}
+					res.status(200).render('message', {
+						message: `<h1>Error</h1><p>No question with an id of ${req.params.id} was found.</p>`,
+						button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
+						pageTitle: 'Error'
+					});
+				}
             }
         } catch (error) {
-					res.status(400).render('message', {
-					message: `<h1>Error</h1><p>No question with an id of ${req.params.id} was found.</p>`,
-					button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
-					pageTitle: 'Error'
+			res.status(400).render('message', {
+			message: `<h1>Error</h1><p>No question with an id of ${req.params.id} was found.</p>`,
+			button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
+			pageTitle: 'Error'
         });
-					console.error(error);
+			console.error(error);
         }
     });
 
@@ -101,11 +101,11 @@ module.exports = ({ data, resetZero, incrementNum}) => {
             });
         } catch (error) {
             res.status(400).render('message', {
-							message: '<h1>Error</h1><p>Something went wrong</p>',
-							button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
-							pageTitle: 'Error'
+				message: '<h1>Error</h1><p>Something went wrong</p>',
+				button: `<a href="/" role="button" class="btn btn-primary">Home</a>`,
+				pageTitle: 'Error'
             });
-					console.error(error);
+			console.error(error);
         }
     });
 
